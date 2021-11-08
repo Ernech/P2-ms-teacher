18fb5d71a6ac22f3d8dc370f30d48ae9b7deb761
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2021-11-08 13:16:41 -0400

    Adding eureka client annotation

--- src/main/java/com/arquitecturasoftware/msteacher/MsTeacherApplication.java
+++ src/main/java/com/arquitecturasoftware/msteacher/MsTeacherApplication.java
@@ -2,8 +2,10 @@ package com.arquitecturasoftware.msteacher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsTeacherApplication {
 
 	public static void main(String[] args) {
